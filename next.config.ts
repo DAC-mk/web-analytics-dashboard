1ad19c07720e310e64a5de9dd344700a058376d8
DAC_SB01 <dac_sb01@dieshikomatsu.local>
2025-10-22 13:17:24 +0900

    Add ESLint ignore during build config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
+  // ESLintエラーを無視する設定
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
